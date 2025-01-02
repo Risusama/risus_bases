11ff0caca4a237a2fbfba5b0609a841a381912b3
Risusama <12206978+Risusama@users.noreply.github.com>
2025-01-02 20:42:46 +0200

    Create mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "Risu's bases";								// name
+//picture = "Mods/TestMod/modpic.tga";				// picture in expanded description
+//logoSmall = "Mods/TestMod/modlogosmall.tga";		// icon next to mod name when description is not expanded
+//logo = "Mods/TestMod/modlogo.tga";				// logo below game menu
+//logoOver = "Mods/TestMod/modlogohover.tga";		// on mouse hover over logo
+tooltip = "Not much to say about this one.";		// tool tip on mouse hover
+//overview = "";									// overview
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=3083277274";	// link
+author = "Risu";									// author
+version = "20240708";								// version (YYYYMMDD of the current Workshop release)
